932e04be6e90d51b77aaddc441e061e3ebc48e9a
Gokul <gokul.b@mail.peoplehum.com>
2023-02-21 00:11:37 +0530

    computedPropertyPros&Cons

--- Cricketer Info/Constants/TeamName.swift
+++ Cricketer Info/Constants/TeamName.swift
@@ -45,3 +45,27 @@ enum TeamName: String, CaseIterable {
         }
     }
 }
+//Computed properties in Swift provide a way to access or modify the value of a property through a function, instead of directly accessing or modifying the value of the property itself. Here are some pros and cons of using computed properties in Swift:
+//
+//Pros:
+//
+//Encapsulation: Computed properties allow you to encapsulate the logic for calculating or deriving a property’s value, making your code more modular and easier to maintain.
+//
+//Composability: Computed properties can be composed with other properties or functions, allowing you to build complex logic on top of simple building blocks.
+//
+//Flexibility: Computed properties can be used to implement properties that would be difficult or impossible to implement with stored properties alone.
+//
+//Efficiency: Computed properties can be optimized by the compiler, so that they perform as well as or better than stored properties.
+//
+//Cons:
+//
+//Overhead: Computed properties require more computational overhead than stored properties, because the getter and/or setter function must be executed every time the property is accessed or modified.
+//
+//Side effects: Computed properties can have side effects that are not immediately apparent, because the getter and/or setter function may perform additional operations beyond simply returning or setting the value of the property.
+//
+//Complexity: Computed properties can make your code more complex and harder to read and understand, especially if they are composed with other properties or functions.
+//
+//Limitations: Computed properties have some limitations, such as not being able to be marked as “lazy” or “weak”.
+//
+//
+//
